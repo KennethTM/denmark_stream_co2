27f9c87a958ea0a58b36fc7a708a4d6106f19b44
Kenneth Thorø Martinsen <kenneth2810@gmail.com>
2023-03-28 15:13:15 +0200

    updated model data

--- modeling.R
+++ modeling.R
@@ -2,10 +2,10 @@ source("libs_and_funcs.R")
 
 model_data_raw <- read_parquet("data/model_data_raw.parquet")
 
-preds <- names(model_data_raw)[31:53]
+preds <- names(model_data_raw)[31:60]
 
 model_data_sub <- model_data_raw |> 
-  filter(snap_dist < 200 & within_lake == 0) |> #remove sites snapping to same src_idx?
+  filter(snap_dist < 100 & within_lake == 0) |> #remove sites snapping to same src_idx?
   select(reachno, co2, all_of(preds))
 
 summary(model_data_sub) #many na's in dk model components
