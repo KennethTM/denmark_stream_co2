a84d72a08596f3b9d7abb73050e36047b8f9c31e
Kenneth Thorø Martinsen <kenneth2810@gmail.com>
2023-10-20 14:31:42 +0200

    Estimated co2 flux.

--- co2_flux.R
+++ co2_flux.R
@@ -9,55 +9,74 @@ wtr_data <- q_points_modeling |>
   select(airt, wtr, season) |> 
   na.omit()
 
-wtr_model <- lm(wtr~airt*season, data = wtr_data)
-anova(wtr_model)
+wtr_model <- lm(wtr~airt, data = wtr_data)
 summary(wtr_model)
 
-#Derive slope
-#Either sample points regularly or just derive slope per stream branch? Or streamnet rasterized from taudem?
-# network <- st_read("rawdata/dk_model_hip_2020.shp")
-# 
-# dist <- 200
-# 
-# network_segment <- st_line_sample(network, density = 1/dist)
-# 
-# network$geometry <- network_segment
-# 
-# network_points <- network |> 
-#   select(BR_BrName) |> 
-#   st_cast("POINT")
-# 
-# dem <- rast("data/dem/dhym.tif")
-# 
-# network_points$z <- extract(dem, vect(network_points), ID = FALSE, raw=TRUE)
-# 
-# network_points_slope <- network_points |> 
-#   group_by(BR_BrName) |> 
-#   mutate(drop = c(NA, diff(z)),
-#          slope = abs(drop)/dist)
+fig_wtr_model <- wtr_data |> 
+  ggplot(aes(x=airt, y=wtr))+
+  geom_abline(slope=1, intercept = 0, linetype=3)+
+  geom_point()+
+  geom_smooth(method="lm", col="coral")+
+  ylim(0, 22)+
+  xlim(0, 22)+
+  xlab(expression("Air temperature ("*degree*"C)"))+
+  ylab(expression("Water temperature ("*degree*"C)"))
 
-#q_points_predictions <- read_parquet("data/q_points_predictions.parquet")
+fig_wtr_model
+
+#Derive slope
+#For now based on entire stream network segments
+network <- st_read("data/dk_model/dk_model_hip_2020.shp")
+dk_border <- st_read("data/dk_border.sqlite")
+network_border <- st_intersection(network, dk_border) |> 
+  st_cast("LINESTRING")
+
+network_begin <- st_startpoint(network_border)
+network_end <- st_endpoint(network_border)
+
+dem <- rast("data/dem/dhym.tif")
+
+begin_elev <- as.numeric(extract(dem, vect(network_begin), raw=TRUE, ID=FALSE, method="bilinear"))
+end_elev <- as.numeric(extract(dem, vect(network_end), raw=TRUE, ID=FALSE, method="bilinear"))
+network_border$drop <- begin_elev - end_elev
+
+network_slope <- network_border |> 
+  mutate(length = as.numeric(st_length(geometry)),
+         slope = abs(drop)/length) |>
+  st_drop_geometry() |> 
+  select(branch = BR_BrName, slope)
+
+#Estimate CO2 flux
+q_points_predictions <- read_parquet("data/q_points_predictions.parquet") |> 
+  filter(snap_dist < 100,
+         in_lake == 0,
+         is.na(downstream))
 
 #Calculate stream velocity from hydraulic geometry relationships
-#q in m3/s
 #raymond 2012
+#q in m3/s, v in m/s
 v_from_q <- function(q){
   v = 0.19 * q^0.29
   return(v)
 }
 
-#raymond 2012
+#raymond 2012, table 2, eq 5
+#v in m/s, slope unitless, temp in celcius, kgas in m/d
 kgas <- function(v, slope, temp){
-  sc <- 1742 - 91.24 * temp + 2.208 * temp^2 - 0.0219 * temp^3
-  kgas <- (v * slope * 2841 + 2.02)*(sc/600)^-0.5
+  sc <- 1742 - 91.24*temp + 2.208*temp^2 - 0.0219*temp^3
+  k600 <- v*slope*2841 + 2.02
+  kgas <- k600*(sc/600)^-0.5
   return(kgas)
 }
 
+#henrys constant from AquaEnv source
+lnK <- function(A, B, C, D, E, T){
+  lnK <- A + (B/T) + C*log(T) + D*T + E*T^2
+  return(lnK)
+}
 
-
-#henrys constant from aquaenv source
-K0_CO2 <- function(S, t)           
-{
+#unit mol/(kg-soln*atm)
+K0_CO2 <- function(S, t){
   T <- t + 273.15
   
   A <- 0.023517*S - 167.81077
@@ -67,12 +86,12 @@ K0_CO2 <- function(S, t)
   E <- 4.7036e-7*S
   
   K0_CO2 <- exp(lnK(A, B, C, D, E, T))
-  #attr(K0_CO2, "unit") <- "mol/(kg-soln*atm)"
-  
+
   return (K0_CO2)
 }
 
 #determine dissolved co2 in uM from temperate
+#temp in celcius, partial pressure in uatm, co2_eq in uM
 co2_eq_from_temp <- function(temp, partial_pressure = 400){
   henry_constant <- K0_CO2(0, temp)
   
@@ -81,14 +100,14 @@ co2_eq_from_temp <- function(temp, partial_pressure = 400){
   return(co2_eq)
 }
 
-
-
-q_points_predictions |> 
-  mutate(wtr_pred = predict(wtr_model, newdata=data.frame(airt=airt, season=season)),
+#calculate flux 
+#flux in unit mmol co2/m2/d
+q_points_flux <- q_points_predictions |> 
+  left_join(network_slope) |> 
+  mutate(wtr_pred = predict(wtr_model, newdata=data.frame(airt=airt)),
          v = v_from_q(discharge),
          kgas = kgas(v, slope, wtr_pred),
          co2_eq = co2_eq_from_temp(wtr_pred),
-         flux = kgas*(co2_pred-co2_eq)*XXXX)
+         flux = kgas*(co2_pred-co2_eq))
 
-
-
+write_parquet(q_points_flux, "data/q_points_flux.parquet")
